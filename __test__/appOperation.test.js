1c33d57519f2a6e499af7d4959cf9217cbf93960
Boniface Musyoki <bonifacemusyoki@gmail.com>
2025-01-29 04:32:03 -0800

    fixed test cases and code coverage

--- __test__/appOperation.test.js
+++ __test__/appOperation.test.js
@@ -1,6 +1,10 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication of zero", ()=>{
-    expect(appOperations(5,0)).toBe(0);
+    expect(appOperations.multiply(5,0)).toBe(0);
+
+})
+test("adding two values", ()=>{
+    expect(appOperations.add(5,5)).toBe(10);
 
 })
